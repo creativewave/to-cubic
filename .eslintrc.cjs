bc636f972350a4d467ddeb38b63ef62d8282fbf5
Guillaume <gui.lebas@gmail.com>
2020-12-25 08:01:13 +0100

    fix(.eslintrc) Use @cdoublev/eslint-config/node for all files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,13 @@
 
 module.exports = {
+    extends: ['@cdoublev/eslint-config/node'],
     overrides: [
         {
             extends: ['@cdoublev/eslint-config/jest'],
             files: ['__tests__/*.js'],
         },
         {
-            extends: ['@cdoublev/eslint-config/browser', '@cdoublev/eslint-config/node'],
+            extends: ['@cdoublev/eslint-config/browser'],
             files: ['src/**/*.js'],
             settings: {
                 polyfills: ['Object'],
